2669ecd8672bfd55df8a5e4af478827a138401a7
LP <lovelesh_p@zohomail.in>
2023-03-14 12:28:26 +0530

    version bumpup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pycryptobox",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
